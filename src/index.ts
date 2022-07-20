c3bcb4f3eb96ac1b637b291a496cdb5477a639bc
jagadhis <jwaran78@gmail.com>
2022-07-20 12:57:40 +0530

    Array In TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 
 var message:string = "hello typescript"
 console.log(message);
+
+var a:number = 27
+console.log(a);
+
+interface Arr{
+    [index:number] : number;
+   
+}
+
+var examples:Arr  = {
+    100:10
+}
+
+console.log(examples)
